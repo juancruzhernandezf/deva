29dfd43dfd43635018630c92905dad74a4c468f7
juancruzhernandezf <juancruzhernandezf@gmail.com>
2022-10-29 15:08:09 -0300

    ADD FOOTER PHP AND INDEX PHP

--- index.php
+++ index.php
@@ -164,19 +164,7 @@
 
     </main>
 
-    <footer>
-        <h6>Suscribite al Newsletter</h6>
-        <form action="newsletter.php" method="post">
-            <input type="email">
-            <input type="submit">
-        </form>
-        <div class="footer_redes-sociales">
-            <a href="https://www.facebook.com/devaesportsarg/"><img src="#" alt="Facebook logo" class="redes-sociales"></a>
-            <a href="https://www.instagram.com/devaesportsarg/"><img src="#" alt="Instagram logo" class="redes-sociales"></a>
-            <a href="https://twitter.com/devaesportsarg"><img src="#" alt="Twitter logo" class="redes-sociales"></a>
-            <a href="https://www.youtube.com/channel/UCV5CADvqp-yeTfAiHUcW_VQ"><img src="#" alt="Youtube logo" class="redes-sociales"></a>
-        </div>
-    </footer>
+    <?php include('footer.php');?>
 
 
 </body>
