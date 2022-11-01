2598b0a6bc38c38944a6e6ed6cf14e654dd89206
yetiboy94 <90594909+yetiboy94@users.noreply.github.com>
2022-11-01 16:29:42 -0300

    Add files via upload

---
+++ academico.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DEVA - Académico</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+
+    <?php include("header.php");?>
+
+    <main>
+
+<!-- componente banner -->
+
+        <div class="banner">
+            <img src="img/img_academico.png" alt="academico">
+            <div class="texto_banner">
+                <h1 class="título_banner">académico</h1>
+                <p class="bajada_banner">aprender sobre E-SPORTS te va a ayudar a expandir tus oportunidades de emprender, crecer y potenciar tu carrera.</p>
+            </div>
+        </div>
+<!-- componente carrousel certificados -->
+
+        <div class="container_carousel">
+                <h2>certificados</h2>
+                <div class="carousel">
+                    <article class="main_carousel-scroll">
+                        <img src="recursos/negocios.png" alt="desarrollo de negocios esports">
+                        <div>
+                            <h3>desarrollo de negocios esports</h3>
+                            <p class="epigrafe">inicio el abril 2023 con modalidad virtual.</p>
+                            <P>El parque temático abrió las puertas de su primera arena gamer totalmente gratuita y abierta al público y sus comunidades.</P>
+                            <a href="certificados/negocios_esports.html" class="carousel-1">
+                                <p>Ver más</p>
+                                <div class="icon-arrow-right"></div>
+                            </a>
+                        </div>
+                    </article>
+                    <article class="main_carousel-scroll">
+                        <h3>desarrollo de videojuegos en 2d</h3>
+                        <p class="epigrafe">inicio el abril 2023 con modalidad virtual.</p>
+                        <P>destinatado para chicos/as y adolescentes entre 12 y 16 años aproximadamente.En este curso adquirirás las habilidades básicas para programar y diseñar tus propios juegos 2D e introducirte en el fascinante mundo del desarrollo de videojuegos. Al finalizar el curso, te llevarás tu propio juego y con las herramientas aprendidas, podrás seguir creando y divirtiéndote en casa.</P>
+                        <a href="certificados/desarrollo.html" class="carousel-1">
+                            <p>Ver más</p>
+                            <div class="icon-arrow-right"></div>
+                        </a>
+                    </article>
+                    <article class="main_carousel-scroll">
+                        <h3>deportes electrónicos y videojuegos</h3>
+                        <p class="epigrafe">inicio el abril 2023 con modalidad virtual.</p>
+                        <p>se trata de una diplomatura de Extensión universitaria abierta a todo público, organizada por la Sede Andina de la Universidad Nacional de Río Negro (UNRN), a través de la Licenciatura en Diseño Artístico Audiovisual, junto a la Asociación de Deportes Electrónicos y Videojuegos de Argentina (DEVA). Brinda la posibilidad de capacitación y aprendizaje, no sólo a los integrantes de la comunidad universitaria sino también a la sociedad en su conjunto.</p>
+                        <a href="certificados/esports_y_videojuegos.html" class="carousel-1">
+                            <p>Ver más</p>
+                            <div class="icon-arrow-right"></div>
+                        </a>
+                    </article>
+                </div>
+        </div>
+
+<!-- Componente Tarjeta "Se parte del equipo" -->
+
+        <div class="main_card_magenta">
+            <h3>Sé parte del equipo</h3>
+            <p>Postulate para un puesto de colaborador voluntario en DEVA, la cual es una Asociación Civil sin fines de lucro.</p>
+            <a href="equipo.html" class="boton_info">
+                <p>+INFO</p>
+                <div class="icon-arrow-right"></div>
+            </a>
+        </div >
+
+<!-- Contenido específico -->
+
+        <div class="tarjeta_negro">
+                <h2>INVESTIGACIONES</h2>
+                <div class="container_carousel">
+                    <div class="carousel">
+                        <article class="main_carousel-scroll">
+                            <img src="recursos/DFIC.png" alt="dirección de formaciíon e investigación científica">
+                            <div class="text_box">
+                                <h3>dirección de formaciíon e investigación científica</h3>
+                                <p class="epigrafe">inicio el abril 2023 con modalidad virtual.</p>
+                                <P>otorgamos credibilidad, marco teórico y rigurosidad científica a los estudios, tesis, trabajos, artículos e investigaciones sobre el Deporte Electrónico y a los Videojuegos en Argentina.</P>
+                                <a href="certificados/negocios_esports.html" class="carousel-1">
+                                    <p>Ver más</p>
+                                    <div class="icon-arrow-right"></div>
+                                </a>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <h2>INVESTIGACIONES</h2>
+                <div class="container_programa_v1">
+                    <img src="img/diplomatura.png" alt="diplomatura:esports y videojuegos">
+                    <article>
+                        <h3>diplomatura: deportes electónicos y videojuegos</h3>
+                        <p class="epigrafe">duración: 6 meses</p>
+                        <p>Se trata de una diplomatura de Extensión universitaria abierta a todo público, organizada por la Sede Andina de la Universidad Nacional de Río Negro (UNRN), a través de la Licenciatura en Diseño Artístico Audiovisual, junto a la Asociación de Deportes Electrónicos y Videojuegos de Argentina (DEVA). Brinda la posibilidad de capacitación y aprendizaje, no sólo a los integrantes de la comunidad universitaria sino también a la sociedad en su conjunto.</p>
+                        <a href="diplomatura/esports_y_videjuegos.html" class="boton_info_cian">
+                            <p>+INFO</p>
+                            <div class="icon-arrow-right"></div> 
+                        </a>
+                    </article>
+                </div><div class="container_programa_v2">
+                    <img src="img/videojuegos_2d.png" alt="desarrollo de videojuegos 2d">
+                    <article>
+                        <h3>desarrollo de videojuegos en 2d</h3>
+                        <p class="epigrafe">duración: 2 meses</p>
+                        <p>Destinatado para chicos/as y adolescentes entre 12 y 16 años aproximadamente.</p>
+                        <p>En este curso adquirirás las habilidades básicas para programar y diseñar tus propios juegos 2D e introducirte en el fascinante mundo del desarrollo de videojuegos. Al finalizar el curso, te llevarás tu propio juego y con las herramientas aprendidas, podrás seguir creando y divirtiéndote en casa.</p>
+                        <a href="desaarrollo_videojueos_2d.html" class="boton_info_cian">
+                            <p>+INFO</p>
+                            <div class="icon-arrow-right"></div> 
+                        </a>
+                    </article>
+                </div><div class="container_programa_v1">
+                    <img src="img/master.png" alt="master en gestión de esports">
+                    <article>
+                        <h3>máster en gestión de esports</h3>
+                        <p class="epigrafe">duración: 13 meses</p>
+                        <p>El objetivo principal del máster es capacitarte en la utilización de los instrumentos de gestión para que puedas reconocer oportunidades de negocio, establecer estrategias, anticiparte a los cambios, optimizar recursos y analizar la información con criterio, sin que el tiempo ni el lugar representen un obstáculo adaptando el ritmo de estudio a tu carga de trabajo individual y tu disponibilidad.</p>
+                        <a href="master_gestion_esports.html" class="boton_info_cian">
+                            <p>+INFO</p>
+                            <div class="icon-arrow-right"></div> 
+                        </a>
+                    </article>
+                </div>
+        </div>
+
+<!-- Hasta acá el contenido específico --> 
+
+        <div class="main_galeria-carousel">
+            <div>
+                <a href="#"><img src="#" alt="anterior foto"></a>
+                <img src="#" alt="Foto1">
+                <a href="#"><img src="#" alt="siguiente foto"></a>
+            </div>
+        </div>
+
+        <div class="main_card-magenta">
+            <h3>Contactate con DEVA</h3>
+                <p>Si estás interesado en saber un poco más de nosotros, o tenés una consulta, estaremos más que comprometidos en responderte lo antes posible.</p>
+                <a href="contacto.html">
+                    <p>+INFO</p>
+                    <div class="icon-arrow-right"></div>
+                </a>
+        </div>
+    </main>
+
+    <?php include('footer.php');?>
+
+    <script src="academico.js"></script>
+
+</body>
+
+</html>
