f542422e82973d4e977e5f1a99c60ca29585b1d3
juancruzhernandezf <juancruzhernandezf@gmail.com>
2022-11-09 17:06:35 -0300

    changes home

--- footer.php
+++ footer.php
@@ -1,10 +1,16 @@
 <footer>
-        <h6>Suscribite al Newsletter</h6>
-        <form action="newsletter.php" method="post">
-            <label for="email">Tu email</label>
-            <input name="email" type="email" placeholder="Ingresa tu correo electrónico">
-            <input type="submit" value="Enviar">
-        </form>
+        
+    <div>
+        <img src="recursos/deva.svg" alt="Logo DEVA" id="logo-deva">
+        <div id="newsletter">
+            <h6>Suscribite al Newsletter</h6>
+            <form action="newsletter.php" method="post">
+                <label for="email">Tu email</label>
+                <input name="email" type="email" placeholder="Ingresa tu correo electrónico">
+                <input type="submit" value="Enviar">
+            
+            </form>
+        </div>
         <ul class="footer_redes-sociales">
             <li>
                 <a href="https://www.facebook.com/devaesportsarg/"><img src="recursos/facebook.svg" alt="Facebook logo" class="redes-sociales"></a>
@@ -19,6 +25,7 @@
             <a href="https://www.youtube.com/channel/UCV5CADvqp-yeTfAiHUcW_VQ"><img src="recursos/youtube.svg" alt="Youtube logo" class="redes-sociales"></a>
             </li>
         </ul>
+        </div>
 
         <p><a href="terms.php">Términos y condiciones</a> | <a href="terms.php">Aviso Legal</a> | <a href="terms.php">Mapa de Sitio</a> | Todos los derechos reservados</p>
         <p> © Asociación de Deportes Electrónicos y Videojuegos (DEVA) 2018 All right reserved.</p>
